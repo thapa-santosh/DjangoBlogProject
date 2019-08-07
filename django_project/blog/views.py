5c6930d399c47bca8bf7fce3f58f49afc68e80da
TheSantoshThapa <santosthapa39@gmail.com>
2019-08-07 20:09:52 +0545

    commit;

---
+++ django_project/blog/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from .models import Post
+
+#demo posts
+
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'about'})
+
+
---
+++ venv/lib/python3.7/_dummy_thread.py
@@ -0,0 +1,1 @@
+/Users/mymac/anaconda3/lib/python3.7/_dummy_thread.py
