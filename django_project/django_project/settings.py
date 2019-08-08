c866bfd125365a63f24489408df017bc66681281
TheSantoshThapa <santosthapa39@gmail.com>
2019-08-08 12:28:39 +0545

    Django-blog

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- django_project/users/apps.py
+++ django_project/users/apps.py
@@ -4,6 +4,9 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     name = 'users'
 
+
     def ready(self):
         import users.signals
+
+
 
